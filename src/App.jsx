ffe4c85fdbe8eddd6aecfdf65d23e545a7699e93
anushka8singh <anushka0506singh@gmail.com>
2025-12-27 21:25:52 +0530

    Added Tailwind CSS v4 and styled Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ function App(){
   return(
     <div>
       <Navbar />
+      <h1 className="text-3xl font-bold text-blue-600">
+  Hello Tailwind 👋
+</h1>
+
       <h1>
         My Portfolio
       </h1>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 function Navbar(){
     return (
-        <nav>
-            <h2>
+        <nav className="flex items-center justify-between px-8 py-4 bg-white shadow">
+            <h2 className="text-xl font-semibold">
                 Anushka
             </h2>
-            <ul>
-                <li>Home</li>
-                <li>Projects</li>
-                <li>Contact</li>
+            <ul className="flex gap-6 text-gray-600">
+              <li className="cursor-pointer hover:text-black">Home</li>
+              <li className="cursor-pointer hover:text-black">Projects</li>
+              <li className="cursor-pointer hover:text-black">Contact</li>  
             </ul>
         </nav>
     );
