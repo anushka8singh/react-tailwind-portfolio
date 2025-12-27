2009182f82873111873f62c31d558bc991666e92
anushka8singh <anushka0506singh@gmail.com>
2025-12-27 21:05:56 +0530

    Created Navbar component using react

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar(){
+    return (
+        <nav>
+            <h2>
+                Anushka
+            </h2>
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
